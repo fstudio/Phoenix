febbb188c16dfead955afa5c9e63b9ec62069c61
Force.Master-I <forcegroup@users.noreply.github.com>
2014-12-04 11:02:52 +0800

    Update Configure.bat

--- Configure.bat
+++ Configure.bat
@@ -4,5 +4,5 @@ rd /s /q "%~dp0include/rapidjson"
 :CopyRapidJson
 echo HELLO
 mkdir "%~dp0include/rapidjson"
-xcopy "%~dp0deps/rapidjson/include/rapidjson" "%~dp0include/rapidjson" /s /e /d
+xcopy "%~dp0deps/rapidjson/include/rapidjson" "%~dp0include/rapidjson" /s /e 
 goto :EOF
