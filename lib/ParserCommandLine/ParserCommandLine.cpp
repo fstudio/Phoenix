e20d461ab08d8fe4191518793536bdfed55f32d2
Master.Lee <fvstudio@outlook.com>
2015-01-06 23:51:06 +0800

    Write ParserCommandLine

--- lib/ParserCommandLine/ParserCommandLine.cpp
+++ lib/ParserCommandLine/ParserCommandLine.cpp
@@ -1,6 +1,68 @@
-#include "ParserCommandLine.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "ParserCommandLine.h"
 
+using namespace Phoenix;
 
+bool ParserCommandLine::ParserPowerShellStyleBegin()
+{
+    return true;
+}
 
+bool ParserCommandLine::ParserPOSIXStyleBegin()
+{
+    return true;
+}
+
+bool ParserCommandLine::ParserDOSStyleBegin()
+{
+    return true;
+}
+
+bool ParserCommandLine::ParserBegin()
+{
+    switch(this->style)
+    {
+        case PCL_POWERSHELL_STYLE:
+        return this->ParserPowerShellStyleBegin();
+        case PCL_POSIX_NORMAL_STYLE:
+        return this->ParserPOSIXStyleBegin();
+        case PCL_DOS_BASPLIT_STYLE:
+        return this->ParserDOSStyleBegin();
+        default:
+        break;
+    }
+    return false;
+}
+
+///////////////////////////////////////////////////////////////////
+bool ParserCommandLineW::ParserPowerShellStyleBegin()
+{
+    return true;
+}
+
+bool ParserCommandLineW::ParserPOSIXStyleBegin()
+{
+    return true;
+}
+
+bool ParserCommandLineW::ParserDOSStyleBegin()
+{
+    return true;
+}
+
+bool ParserCommandLineW::ParserBegin()
+{
+    switch(this->style)
+    {
+        case PCL_POWERSHELL_STYLE:
+        return this->ParserPowerShellStyleBegin();
+        case PCL_POSIX_NORMAL_STYLE:
+        return this->ParserPOSIXStyleBegin();
+        case PCL_DOS_BASPLIT_STYLE:
+        return this->ParserDOSStyleBegin();
+        default:
+        break;
+    }
+    return false;
+}
--- lib/ParserCommandLine/ParserCommandLine.h
+++ lib/ParserCommandLine/ParserCommandLine.h
@@ -1,33 +1,65 @@
 ////
-#include<string>
+#include <string>
 #include <map>
-//T->char T->wchar_t
-template<class T>
+#include <vector>
+#include <functional>
+
+namespace Phoenix{
+    enum PARSER_CMDLINE_STYLE
+    {
+    PCL_POSIX_NORMAL_STYLE=1,/// -key=value.
+    PCL_POWERSHELL_STYLE=2,/// -File D:/OPT/helloworld.cpp -Reset -Force
+    PCL_DOS_BASPLIT_STYLE=3 //// /? /help.
+    };
+}
+
 class ParserCommandLine{
 private:
-	int m_Argc;
-	char **m_Argv;
+    int m_Argc;
+    char **m_Argv;
     int dwError;
-	std::map<std::string,std::string> OpCode;
-	std::vector<std::string> OpValue;
+    std::map<std::string,std::string> OpCode;
+    std::map<std::string,std::function<int(std::string)>> Task;
+    std::vector<std::string> OpValue;
+    unsigned style;
 public:
+    void SetParserStyle(unsigned i)
+    {
+		this->style=i;
+    }
     ParserCommandLine(int Argc,char **Argv):dwError(0)
-     {
+    {
         //
-     }
+    }
+    bool ParserBegin();
+private:
+    bool ParserPowerShellStyleBegin();
+    bool ParserPOSIXStyleBegin();
+    bool ParserDOSStyleBegin();
 };
 
 
 class ParserCommandLineW{
 private:
-	int m_Argc;
-	wchar_t **m_Argv;
+    int m_Argc;
+    wchar_t **m_Argv;
     int dwError;
-	std::map<std::wstring,std::wstring> OpCode;
-	std::vector<std::wstring> OpValue;
+    std::map<std::wstring,std::wstring> OpCode;
+    std::map<std::string,std::function<int(std::wstring)>> Task;
+    std::vector<std::wstring> OpValue;
+    unsigned style;
 public:
-    ParserCommandLine(int Argc,wchar_t **Argv):dwError(0)
-     {
+    void SetParserStyle(unsigned i)
+    {
+        this->style=i;
+    }
+    ParserCommandLineW(int Argc,wchar_t **Argv):dwError(0)
+    {
         //
-     }
+    }
+    bool ParserBegin();
+private:
+    bool ParserPowerShellStyleBegin();
+    bool ParserPOSIXStyleBegin();
+    bool ParserDOSStyleBegin();
 };
