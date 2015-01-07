a0bf3e0d36f3be3826c406e79b333704fd1283cd
Force.Master-I <forcegroup@users.noreply.github.com>
2015-01-07 12:32:19 +0800

    Update Pclw.cpp

--- test/ParserCmdLine/Pclw.cpp
+++ test/ParserCmdLine/Pclw.cpp
@@ -1,9 +1,9 @@
-#include "ParserCommandLine.h"
+#include "ParserCommandLine.hpp"
 
 int wmain(int argc,wchar_t** argv)
 {
 	ParserCommandLineW cmd(argc,argv);
 	cmd.SetParserStyle(Phoenix::PCL_POSIX_NORMAL_STYLE);
-	cmd.ParserStart();
+	cmd.ParserBegin();
 	return 0;
 }
