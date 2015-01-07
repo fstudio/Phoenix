264e9672e8765c0466edc933edfab6ed7e3455ba
Force.Master-I <forcegroup@users.noreply.github.com>
2015-01-07 19:02:25 +0800

    Update Pcl.cpp

--- test/ParserCmdLine/Pcl.cpp
+++ test/ParserCmdLine/Pcl.cpp
@@ -1,9 +1,19 @@
 #include "ParserCommandLine.hpp"
+#include <iostream>
+
+int TaskTest(std::string ab)
+{
+	std::cout<<ab<<std::endl;
+	return 0;
+}
+
 
 int main(int argc,char** argv)
 {
 	ParserCommandLine cmd(argc,argv);
 	cmd.SetParserStyle(Phoenix::PCL_POSIX_NORMAL_STYLE);
+	cmd.PushCommandTask(TaskTest,"-help");
 	cmd.ParserBegin();
+	cmd.ExecuteTask();
 	return 0;
 }
