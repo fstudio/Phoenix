a5ede963604892580ae8d6c20dc199021a7e3647
Force.Charlie <fvstudio@outlook.com>
2015-02-13 23:27:06 +0800

    Remove ParserCommandLine

--- lib/WindowsRuntimeLibrary/Phoenix.Windows.Runtime.cpp
+++ lib/WindowsRuntimeLibrary/Phoenix.Windows.Runtime.cpp
@@ -1,9 +1,9 @@
 /*********************************************************************************************************
 * Phoenix.Windows.Runtime.h
-* Copyright (C) 2014 The ForceStudio All Rights Reserved.
 * Note: IShell Common Dialog.
 * E-mail:<forcemz@outlook.com>
-* @2014.08
+* Data: @2014.08
+* Copyright (C) 2015 The ForceStudio All Rights Reserved.
 **********************************************************************************************************/
 #include "Phoenix.Windows.Runtime.h"
 using namespace Phoenix;
