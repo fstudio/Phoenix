d58a4246dcca1272877485ff0c767ba89fe6e024
Force.Charlie <fvstudio@outlook.com>
2015-02-24 21:50:19 +0800

    add parsercommand test

--- test/ParseCommandLine/ParseCommandLine.hpp
+++ test/ParseCommandLine/ParseCommandLine.hpp
@@ -1,13 +1,16 @@
 #ifndef PARSECOMMANDLINE_HPP
 #define PARSECOMMANDLINE_HPP
 #include <string>
+#include <map>
 
 template <typename CharT=char>
 class ParseCommandLine{
 public:
-    typedef basic_string<CharT> StringRef;
+    typedef std::basic_string<CharT> StringRef;
 private:
     StringRef selfcmd;
+    std::map<StringRef,StringRef> oM;
+    std::map<StringRef,StringRef> *current;
 public:
     ParseCommandLine()
     {
@@ -18,6 +21,15 @@ public:
         if(Argc<1||Argv==nullptr)
             return false;
         this->selfcmd=Argv[0];//Push argv[0] to pragma self command.
+        decltype(Argc) index=1;
+        bool OpSTA=false;
+        while(index <Argc)
+        {
+            if(Argv[i][0]=='-')
+            {
+                OpSTA=true;
+            }
+        }
         return true;
     }
 };
