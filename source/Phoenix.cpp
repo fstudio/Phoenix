36fe9ee72d95fe9fb777c7318f2efcf580df70bf
Force.Charlie <fvstudio@outlook.com>
2015-02-28 23:33:20 +0800

    ForceHANDLE class fix

--- source/Phoenix.cpp
+++ source/Phoenix.cpp
@@ -24,26 +24,44 @@ static std::map<int,HINSTANCE> ChildProcessMap;
 
 class ForceHANDLE{
 public:
-    HANDLE hAndle;
+    HANDLE m_handle;
     ForceHANDLE()
     {
-        hAndle=nullptr;
+        m_handle=nullptr;
+    }
+    ForceHANDLE(HANDLE hPtr)
+    {
+        if(hPtr)
+            this->m_handle=hPtr;
+    }
+    HANDLE Get() {
+        return this->m_handle;
     }
     ~ForceHANDLE()
     {
-        if(hAndle)
+        if(m_handle)
         {
-            CloseHandle(hAndle);
-            hAndle=nullptr;
+            CloseHandle(m_handle);
+            m_handle=nullptr;
         }
     }
     bool Delete()
     {
-        if(hAndle)
+        if(m_handle)
         {
-            CloseHandle(hAndle);
-            hAndle=nullptr;
+            CloseHandle(m_handle);
+            m_handle=nullptr;
         }
+    }
+    ForceHANDLE& operator=(HANDLE hPtr)
+    {
+        m_handle=static_cast<HANDLE>(hPtr);
+        return *this;
+    }
+    ForceHANDLE& operator=(const ForceHANDLE &forceHandle)
+    {
+        this->m_handle=static_cast<HANDLE>(forceHandle.Get());
+        return *this;
     }
 };
 
