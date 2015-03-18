0edb05e19d526924658cb1eb1d59c1270f9ac467
Force.Charlie <fvstudio@outlook.com>
2015-03-18 23:00:43 +0800

    Standard IO Console Feature add

---
+++ source/PhoenixStandardIO.cpp
@@ -0,0 +1,47 @@
+/*********************************************************************************************************
+* PhoenixStandardIO.cpp
+* Note: Phoenix Standard IO
+* E-mail:<forcemz@outlook.com>
+* Data: @2015.03
+* Copyright (C) 2015 The ForceStudio All Rights Reserved.
+**********************************************************************************************************/
+#include "Header.hpp"
+#include <stdio.h>
+
+class StandardIO{
+private:
+    HANDLE hStd;
+public:
+    StandardIO();
+    ~StandardIO();
+    bool Input();
+    bool Output();
+};
+
+BOOL StandardMain(int Argc,wchar_t **Argv)
+{
+    //
+    BOOL bRet=TRUE;
+    if((bRet=AttachConsole(ATTACH_PARENT_PROCESS))!=TRUE){
+        return bRet;
+    }
+    freopen("CONIN$" , "r+t" , stdin);
+    freopen("CONOUT$" , "w+t" , stdout);
+    freopen("CONOUT$", "w", stderr);
+    return bRet;
+}
+
+BOOL CloseStandardIO()
+{
+    fclose(stdout);
+    fclose(stdin);
+    fclose(stderr);
+    return TRUE;
+}
+
+bool CreateConsoleWindow()
+{
+    if(AllocConsole()!=TRUE)
+        return false;
+    return true;
+}
