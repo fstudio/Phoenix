3aa6ae118ffdc9dbb1e518679a3a902667434bb1
Force.Charlie <fvstudio@outlook.com>
2015-03-25 22:03:46 +0800

    fix idl file

--- tools/Container/ContainerLPCClient.cc
+++ tools/Container/ContainerLPCClient.cc
@@ -16,3 +16,14 @@ int WINAPI LauncherWithLPC(LPCWSTR pszApp,
 {
     return 0;
 }
+
+
+void __RPC_FAR* __RPC_USER midl_user_allocate(size_t len)
+{
+     return(malloc(len));
+}
+
+void __RPC_USER midl_user_free(void __RPC_FAR *ptr)
+{
+     free(ptr);
+}
