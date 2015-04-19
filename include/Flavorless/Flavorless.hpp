8789c976b6a41269d31a321278e1564da68ec47a
Force.Charlie <fvstudio@outlook.com>
2015-04-19 23:22:27 +0800

    Flavorless library add

---
+++ include/Flavorless/Flavorless.hpp
@@ -0,0 +1,11 @@
+/*********************************************************************************************************
+* Flavorless.hpp
+* Note: Phoenix Flavorless Library
+* Date: @2015.04
+* E-mail:<forcemz@outlook.com>
+* Copyright (C) 2015 The ForceStudio All Rights Reserved.
+**********************************************************************************************************/
+#ifndef FLAVORLESS_LIBRARY_HPP
+#define FLAVORLESS_LIBRARY_HPP
+
+#endif
---
+++ include/Flavorless/ReadMe.md
@@ -0,0 +1,5 @@
+#Flavorless Ini Parser Module
+And meal ticket the identity of farewell, Say end your
+
+###What's fuck Flavorless Library
+This is a fast ini parser library,
