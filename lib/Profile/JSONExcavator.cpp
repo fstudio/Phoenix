f3f2ea6b2d0c92b43df5bcb46f197ffe5aad1d2b
Force.Charlie-I <forcegroup@users.noreply.github.com>
2015-04-28 13:37:53 +0800

    Create JSONExcavator.cpp

---
+++ lib/Profile/JSONExcavator.cpp
@@ -0,0 +1,14 @@
+/*********************************************************************************************************
+*   Phoneix JSON Excavator
+*   Note: JSONExcavator.cpp
+*   Date: 2015.04.28
+*   Author: Force.Charlie
+*   E-mail: <forcemz@outlook.com>
+*   Copyright (C) 2015 ForceStudio.All Rrights Reserved.
+**********************************************************************************************************/
+#include <Windows.h>
+
+class JSONExcavator{
+public:
+  JSONExcavator(){}
+};
