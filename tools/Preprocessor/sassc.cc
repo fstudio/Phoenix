220f6bf5071dc27d79ddd658e394ac1667b9ada9
Force.Charlie <fvstudio@outlook.com>
2015-04-28 23:03:20 +0800

    RPC Library add

---
+++ tools/Preprocessor/sassc.cc
@@ -0,0 +1,18 @@
+/*********************************************************************************************************
+* sass.cc
+* Note: Phoenix Preprocessor Sass compiler
+* Date: @2015.04
+* E-mail:<forcemz@outlook.com>
+* Copyright (C) 2015 The ForceStudio All Rights Reserved.
+**********************************************************************************************************/
+#include <cstdio>
+#include <cstdlib>
+#include <sass2scss.h>
+#include <sass_context.h>
+
+
+int main(int argc,char **argv)
+{
+    return 0;
+}
+
