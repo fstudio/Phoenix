0e832915200be1d4ec5d989290d26e87423727b6
Force.Charlie <fvstudio@outlook.com>
2015-05-02 10:26:20 +0800

    add Panel code template

--- tools/Preprocessor/sassc.cc
+++ tools/Preprocessor/sassc.cc
@@ -9,11 +9,25 @@
 #include <cstdlib>
 #include <sass2scss.h>
 #include <sass_context.h>
+#define STRIP_FLAG_HELP 1
 #include <gflags/gflags.h>
+
+using GFLAGS_NAMESPACE::SetUsageMessage;
+using GFLAGS_NAMESPACE::ParseCommandLineFlags;
+using GFLAGS_NAMESPACE::int32;
+using GFLAGS_NAMESPACE::FlagRegisterer;
+using GFLAGS_NAMESPACE::StringFromEnv;
+using GFLAGS_NAMESPACE::RegisterFlagValidator;
+using GFLAGS_NAMESPACE::CommandLineFlagInfo;
+using GFLAGS_NAMESPACE::GetAllFlags;
+
+
+DEFINE_bool(service,false,"Run Sassc With RPC Service");
 
 int main(int argc,char **argv)
 {
-    ::google::ParseCommandLineFlags(&argc,&argv,true);
+    SetUsageMessage("Phoenix Sassc Preprocessor Engine Usage:");
+    ParseCommandLineFlags(&argc,&argv,true);
     return 0;
 }
 
