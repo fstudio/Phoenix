cffd648eccff48dfc5b15fca4214c2b449a4cb70
Force.Charlie <fvstudio@outlook.com>
2015-05-18 22:56:20 +0800

    whois tools is ok

--- tools/whois/whois_new.cpp
+++ tools/whois/whois_new.cpp
@@ -15,19 +15,16 @@
 #include <ws2tcpip.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
+#include <codecvt>
+#include <vector>
+#pragma comment(lib,"ws2_32")
 
-static wchar_t* whoisList[]={
-    L"whois.internic.net",
-    L"whois.edu.cn",
-    L"whois.cnnic.net.cn",
-    L"whois.arin.net",
-    L"whois.apnic.net",
-    L"whois.ripe.ne",
-    L"whois.twnic.net",
-    L"whois.nic.ad.jp",
-    L"whois.krnic.net",
-    L"whois.lacnic.net"
-};
+
+static wchar_t whoisList[] =L"whois.internic.net";
+wchar_t *host = nullptr;
+wchar_t other[2048] = { 0 };
+int optset = 0;
 
 static bool Whois(const wchar_t *domian);
 static void usage()
@@ -35,50 +32,124 @@ static void usage()
     printf("usage: whois [-h hostname] name ....\n");
 }
 
-int wmain(int argc,wchar_t **argv)
+bool StringToWideString(const std::string& src, std::wstring &wstr)
 {
-    argc--;
-    argv++;
-    if(!argc){
+    std::locale sys_locale("");
+    const char* data_from = src.c_str();
+    const char* data_from_end = src.c_str() + src.size();
+    const char* data_from_next = 0;
+
+    wchar_t* data_to = new wchar_t[src.size() + 1];
+    wchar_t* data_to_end = data_to + src.size() + 1;
+    wchar_t* data_to_next = 0;
+
+    wmemset(data_to, 0, src.size() + 1);
+
+    typedef std::codecvt<wchar_t, char, mbstate_t> convert_facet;
+    mbstate_t in_state = { 0 };
+    auto result = std::use_facet<convert_facet>(sys_locale).in(
+        in_state, data_from, data_from_end, data_from_next,
+        data_to, data_to_end, data_to_next);
+    if (result == convert_facet::ok)
+    {
+        wstr = data_to;
+        delete[] data_to;
+        return true;
+    }
+    delete[] data_to;
+    return false;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
         usage();
         return 0;
     }
-    return 0;
-}
-
-static bool Whois(const wchar_t *domian){
-    int iResult;
-    INT iRetval;
+    host = whoisList;
+    std::vector<std::string> domainV;
+    for (int i = 1; i < argc; i++){
+        switch (argv[i][0]){
+        case '-':
+        case '/':
+            if (strlen(argv[i]) < 2)break;
+            if (strcmp(&argv[i][1], "h") == 0){
+                if (i + 2 < argc)
+                {
+                    std::string str = argv[i + 1];
+                    std::wstring wstr;
+                    if (!StringToWideString(str, wstr))
+                        return 1;
+                    wcsncpy_s(other, wstr.c_str(), 2048);
+                    host = other;
+                    i++;
+                }
+            }
+            break;
+        default:
+            domainV.push_back(argv[i]);
+            break;
+        }
+    }
+    char ch;
     DWORD dwRetval;
     ADDRINFOW *result = NULL;
-    ADDRINFOW *ptr = NULL;
     ADDRINFOW hints;
-    ZeroMemory( &hints, sizeof(hints));
+    ZeroMemory(&hints, sizeof(hints));
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_protocol = IPPROTO_TCP;
     WSADATA wsaData;
     auto iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    if((dwRetval=GetAddrInfoW(whoisList[0],nullptr,&hints,&result))!=0){
+    if ((dwRetval = GetAddrInfoW(host, nullptr, &hints, &result)) != 0){
         wprintf(L"whois: GetAddrInfoW failed with error: %d\n", dwRetval);
         WSACleanup();
         return 1;
     }
-    ////
-    for(ptr=result; ptr != NULL ;ptr=ptr->ai_next) {
-        switch (ptr->ai_family) {
-            case AF_UNSPEC:
-            wprintf(L"Unspecified\n");
-            break;
-            case AF_INET:
-            break;
-            case AF_INET6:
-            break;
-            default:
-            wprintf(L"Other %ld\n", ptr->ai_family);
-            break;
-        }
+    if (result == nullptr)
+        return 1;
+    SOCKET sock;
+    sock = socket(result->ai_family, SOCK_STREAM, IPPROTO_TCP);
+    if (sock == INVALID_SOCKET) {
+        wprintf(L"socket function failed with error: %ld\n", WSAGetLastError());
+        FreeAddrInfoW(result);
+        WSACleanup();
+        return 1;
     }
-    //auto s=socket()
-    return true;
+    wchar_t ipstringbuffer[46];
+    DWORD ipbufferlength = 46;
+    WSAAddressToString(result->ai_addr, (DWORD)result->ai_addrlen, NULL,
+        ipstringbuffer, &ipbufferlength);
+    sockaddr_in sin;
+    memset(/*(caddr_t)*/&sin, 0, sizeof(sin));
+    sin.sin_family = result->ai_family;
+    ULONG iaddr;
+    InetPtonW(result->ai_family, ipstringbuffer, &iaddr);
+    sin.sin_addr.s_addr = iaddr;
+    sin.sin_port = htons(43);
+    iResult = connect(sock, (sockaddr *)&sin, sizeof(sin));
+    if (iResult == SOCKET_ERROR) {
+        wprintf(L"connect function failed with error: %ld\n", WSAGetLastError());
+        iResult = closesocket(sock);
+        if (iResult == SOCKET_ERROR)
+            wprintf(L"closesocket function failed with error: %ld\n", WSAGetLastError());
+        FreeAddrInfoW(result);
+        WSACleanup();
+        return 1;
+    }
+    auto i = domainV.size();
+    while (i-- > 1){
+        auto dm = domainV[i-1];
+        send(sock, dm.c_str(), dm.size(), 0);
+        send(sock, " ", 1, 0);
+    }
+    send(sock, domainV[0].c_str(), domainV[0].size(), 0);
+    send(sock, "\r\n", 2, 0);
+    wprintf(L"[%s]\n", host);
+    while (recv(sock, &ch, 1, 0) == 1)
+        putchar(ch);
+    FreeAddrInfoW(result);
+    WSACleanup();
+    return 0;
 }
