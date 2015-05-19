c02dab9b9c094eba9d371198d7a3819fbe988fb1
Force.Charlie <fvstudio@outlook.com>
2015-05-20 00:14:07 +0800

    add byacc prot and bzip and PythonRuntime.cc

---
+++ tools/Packers/tools/NEnv/PythonRuntime.cc
@@ -0,0 +1,41 @@
+/*********************************************************************************************************
+* PythonRuntime.cc
+* Note: Phoenix Container Runtime for Python
+* Date: @2015.05
+* Author: Force Charlie
+* E-mail:<forcemz@outlook.com>
+* Copyright (C) 2015 The ForceStudio All Rights Reserved.
+**********************************************************************************************************/
+#include <Windows.h>
+#include <Python.h>
+
+
+class MBGet{
+private:
+    char *p;
+public:
+    MBGet(const wchar_t *str){}
+    ~MBGet(){
+        if(p)
+        {
+            free(p);
+        }
+    }
+    const char* get(){return this->p;}
+};
+
+int RunPythonFunction(const wchar_t *filePath,const wchar_t *fun)
+{
+    Py_Initialize();
+    if(!Py_IsInitialized())
+        return -1;
+    PyObject *pModule=nullptr;
+    PyObject *pFunc=nullptr;
+    MBGet filePathA(filePath);
+    MBGet funA(fun);
+    pModule=PyImport_InportModule(filePathA.get());
+    pFunc=PyObject_GetAttrString(pModule,funA.get());
+    PyEval_CallObject(pFunc,nullptr);
+    Py_Finalize();
+    return 0;
+}
