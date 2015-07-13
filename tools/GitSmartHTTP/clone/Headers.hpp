9b6206151d492678086be1355de0b652c60bd26c
Force.Charlie <fvstudio@outlook.com>
2015-07-13 22:26:08 +0800

    add HTTP Header Parser

--- tools/GitSmartHTTP/clone/Headers.hpp
+++ tools/GitSmartHTTP/clone/Headers.hpp
@@ -5,14 +5,47 @@
 #define HTTP_HEADERS_HPP
 #include <string>
 #include <unordered_map>
+
+
+/*
+
+HTTP 200 OK\r\n
+Content-Type: text/plan\r\n
+*/
+
+
 class HTTPHeaders{
 private:
     int responseCode;
+    typedef std::unordered_map<std::wstring,std::wstring> StoreMap;
     std::unordered_map<std::wstring,std::wstring> umap;
+    static bool inline HeaderKVSplit(const wchar_t *buffer,StoreMap &um)
+    {
+        return true;
+    }
 public:
     HTTPHeaders(std::wstring &header)
     {
-        //////
+        if(header.empty())
+            return ;
+        auto msize=header.size();
+        auto begin=header.c_str();
+        const wchar_t *index=begin;
+        const wchar_t *pre=index;
+        const wchar_t *end=begin+msize;
+        bool cr=false;
+        wchar_t buffer[4096]={0};
+        while(index++<end)
+        {
+            if(*index=='\r')
+                cr=true;
+            else
+                cr=false;
+            if(cr&&*index=='\n')
+            {
+                wcsncpy(buffer,pre,index-pre-2);
+            }
+        }
     }
     int GetResponseCode(){
         return this->responseCode;
