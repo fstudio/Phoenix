e8bee7560e4fab2bc68001a44401d4b36e041262
Force.Charlie-I <fcharlie@users.noreply.github.com>
2015-07-16 19:05:48 +0800

    add

--- tools/GitSmartHTTP/git-http-backend.cpp
+++ tools/GitSmartHTTP/git-http-backend.cpp
@@ -1,12 +1,15 @@
-
-
+/*************************************************************************************************************************
+* git-http-backend a smart git http server run on windows use c++
+* Author: Force.Charlie
+* Copyright (C) 2015. All Rights Reserved.
+*************************************************************************************************************************/
 #include <Windows.h>
 #include <string>
 #include <winhttp.h>
 #include <wincrypt.h>
+#include "getopt.h"
 
-
-int wmain()
+int wmain(int argc,wchar_t **argv)
 {
     return 0;
 }
